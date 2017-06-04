f0f77a7b9cdfa99c07da94013b65369694d3d423
Johanna Staedel <bluecherryme@gmail.com>
2017-06-04 16:50:56 -0600

    finished collapsable navbar for small screen

--- WideOpenTechClone/JS/javascript.js
+++ WideOpenTechClone/JS/javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
     $("#hamburger").click(function(){
-        $(".menu-list").toggleClass('invisible');
+        $(".navbar").toggleClass('visible');
     });
     
     
